c744d19d3f42c341148d03dc7203b4899baff248
highTowerSU <3651850+highTowerSU@users.noreply.github.com>
2025-12-08 02:38:38 +0100

    changed to ts

--- index.js
+++
@@ -1,7 +0,0 @@
-const { SevDesk } = require('./dist/nodes/SevDesk/SevDesk.node');
-const { SevDeskApi } = require('./dist/credentials/SevDeskApi.credentials');
-
-module.exports = {
-  nodes: [SevDesk],
-  credentials: [SevDeskApi],
-};
